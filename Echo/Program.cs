610e63314b6582012b5efb2b00ba7a52d45d96c6
mc0re <mnikitin0@gmail.com>
2023-07-18 22:43:42 +0200

    Trivial echo

---
+++ Echo/Program.cs
@@ -0,0 +1,11 @@
+﻿while (true)
+{
+    Console.Write("Enter your name > ");
+    
+    var name = Console.ReadLine();
+    if (string.IsNullOrEmpty(name)) break;
+
+    Console.WriteLine($"Hello, {name}.");
+}
+
+Console.WriteLine("Bye.");
